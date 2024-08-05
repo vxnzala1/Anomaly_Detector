86813e6af7d51c206ca837b72fb657a17810affd
Antonio Venzalá <i02vesea@uco.es>
2024-08-05 12:34:00 +0200

    update

--- IA/Mod1.py
+++ IA/Mod1.py
@@ -4,4 +4,5 @@ from ultralytics import YOLO
 model = YOLO('yolov5s.pt')  # Puedes cambiar a 'yolov5m.pt' o 'yolov5l.pt' para versiones más grandes
 
 # Entrenar el modelo
-model.train(data='/path/to/data.yaml', epochs=100)
+model.train(data='D:\Documentos\Estudios\UNIVERSIDAD\Otros\TFG\Anomaly_Detector\IA\data.yaml', epochs=100)
+
