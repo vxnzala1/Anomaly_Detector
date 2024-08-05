5a8e6693ee6a7549323c8deb140d9685f47c36d7
Antonio Venzalá <i02vesea@uco.es>
2024-08-05 19:28:10 +0200

    updte train

--- IA/Mod1.py
+++ IA/Mod1.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # Puedes cambiar a 'yolov5m.pt' o 'yolov5l.pt' para versiones más grandes
 
 # Entrenar el modelo
-model.train(data=r'D:\Documentos\Estudios\UNIVERSIDAD\Otros\TFG\Anomaly_Detector\IA\data.yaml', epochs=1)
+model.train(data=r'D:\Documentos\Estudios\UNIVERSIDAD\Otros\TFG\Anomaly_Detector\IA\data.yaml', epochs=10) # Varía los epochs según la cantidad de datos y la potencia de tu GPU
